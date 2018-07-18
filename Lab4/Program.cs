5b65185822f458be2c72af229acf7a6e2a706e56
Jacob Hands <jrhands@gmail.com>
2018-07-18 13:46:17 -0400

    Initial commit, made solution and formatted header for table

---
+++ Lab4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0,0}{1,15}{2,15}", "Number", "Squared", "Cubed");
+            Console.WriteLine("{0,0}{1,14}{2,16}", "=======", "=======", "======");
+            Console.Read();
+        }
+    }
+}
