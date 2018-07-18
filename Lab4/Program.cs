8fcc388c66a66dbef3ca710e664d39cf99519798
Jacob Hands <jrhands@gmail.com>
2018-07-18 14:02:31 -0400

    Added all functionality and formatting.

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -6,9 +6,52 @@ namespace Lab4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("{0,0}{1,15}{2,15}", "Number", "Squared", "Cubed");
-            Console.WriteLine("{0,0}{1,14}{2,16}", "=======", "=======", "======");
+            Console.WriteLine("Learn your squares and cubes!");
+            bool repeat = false;
+            do
+            {
+                int userInput = TakeInput();
+                DisplayTable(userInput);
+                repeat = RequestRepeat();
+            } while (repeat);
+
+            Console.WriteLine("\nGoodbye!");
             Console.Read();
+        }
+
+        static int TakeInput()
+        {
+            Console.Write("\nEnter an integer: ");
+            return Int32.Parse(Console.ReadLine());
+        }
+
+        static void DisplayTable(int userInput)
+        {
+            Console.WriteLine("\n{0,-15}{1,-15}{2,-15}", "Number", "Squared", "Cubed");
+            Console.WriteLine("{0,-15}{1,-15}{2,-15}", "=======", "=======", "======");
+            for (int i = 1; i <= userInput; i++)
+            {
+                Console.WriteLine("{0,-15}{1,-15}{2,-15}", i, (i * i), (i * i * i));
+            }
+        }
+
+        static bool RequestRepeat()
+        {
+            Console.Write("\nContinue? (y/n): ");
+            String userInput = Console.ReadLine().ToLower();
+            if (userInput == "y" || userInput == "yes")
+            {
+                return true;
+            }
+            else if (userInput == "n" || userInput == "no")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+                return RequestRepeat();
+            }
         }
     }
 }
